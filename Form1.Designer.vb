77a441be5dc0de539a23ac5b3a449854857c3f63
Polinadrumeva <polina9230@gmail.com>
2025-05-18 14:26:40 +0300

    add basic part of project

---
+++ Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pnl = New System.Windows.Forms.Panel()
+        Me.btnNextQuestion = New System.Windows.Forms.Button()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.lblQuestion = New System.Windows.Forms.Label()
+        Me.responseTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.pnl.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnl
+        '
+        Me.pnl.BackColor = System.Drawing.Color.RosyBrown
+        Me.pnl.Controls.Add(Me.btnNextQuestion)
+        Me.pnl.Controls.Add(Me.RadioButton4)
+        Me.pnl.Controls.Add(Me.RadioButton3)
+        Me.pnl.Controls.Add(Me.RadioButton2)
+        Me.pnl.Controls.Add(Me.RadioButton1)
+        Me.pnl.Controls.Add(Me.lblQuestion)
+        Me.pnl.Location = New System.Drawing.Point(29, 19)
+        Me.pnl.Name = "pnl"
+        Me.pnl.Size = New System.Drawing.Size(745, 419)
+        Me.pnl.TabIndex = 0
+        '
+        'btnNextQuestion
+        '
+        Me.btnNextQuestion.BackColor = System.Drawing.Color.Gray
+        Me.btnNextQuestion.Font = New System.Drawing.Font("Microsoft New Tai Lue", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNextQuestion.Location = New System.Drawing.Point(495, 307)
+        Me.btnNextQuestion.Name = "btnNextQuestion"
+        Me.btnNextQuestion.Size = New System.Drawing.Size(182, 44)
+        Me.btnNextQuestion.TabIndex = 5
+        Me.btnNextQuestion.Text = "Next Question"
+        Me.btnNextQuestion.UseVisualStyleBackColor = False
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Font = New System.Drawing.Font("Microsoft New Tai Lue", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton4.Location = New System.Drawing.Point(436, 208)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(128, 24)
+        Me.RadioButton4.TabIndex = 4
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "RadioButton4"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Font = New System.Drawing.Font("Microsoft New Tai Lue", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton3.Location = New System.Drawing.Point(436, 168)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(128, 24)
+        Me.RadioButton3.TabIndex = 3
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "RadioButton3"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Microsoft New Tai Lue", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(111, 208)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(128, 24)
+        Me.RadioButton2.TabIndex = 2
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "RadioButton2"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Microsoft New Tai Lue", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(111, 168)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(128, 24)
+        Me.RadioButton1.TabIndex = 1
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'lblQuestion
+        '
+        Me.lblQuestion.AutoSize = True
+        Me.lblQuestion.Font = New System.Drawing.Font("Microsoft New Tai Lue", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblQuestion.Location = New System.Drawing.Point(106, 66)
+        Me.lblQuestion.Name = "lblQuestion"
+        Me.lblQuestion.Size = New System.Drawing.Size(74, 27)
+        Me.lblQuestion.TabIndex = 0
+        Me.lblQuestion.Text = "Label1"
+        Me.lblQuestion.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lblQuestion.UseWaitCursor = True
+        '
+        'responseTimer
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.pnl)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.pnl.ResumeLayout(False)
+        Me.pnl.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnl As Panel
+    Friend WithEvents btnNextQuestion As Button
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents lblQuestion As Label
+    Friend WithEvents responseTimer As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,114 @@
+﻿Public Class Form1
+
+    Dim currentQuestion As Integer = 0
+    Dim humanScore As Integer = 0
+    Dim aiScore As Integer = 0
+    Dim responseTime As Integer = 0
+    Dim fastAnswerDetected As Boolean = False
+
+    Dim questions As String() = {
+    "What would you do if you saw a puppy stuck in a drain?",
+    "Which word best defines 'humanity'?",
+    "Complete the phrase: 'Roses are red, violets are blue, ______.'",
+    "Describe rain in one word.",
+    "What is 2 + 2?",
+    "Someone drops their wallet in front of you. What do you do?",
+    "Pick a favorite smell:",
+    "How do you feel when someone cries in front of you?"
+    }
+
+    Dim answers As String(,) = {
+    {"Rescue it immediately", "Take a photo", "Ignore it", "Analyze the situation"},
+    {"Logic", "Compassion", "Efficiency", "Calculation"},
+    {"algorithms are precise", "flowers bloom", "someone loves you", "system error"},
+    {"peaceful", "wet", "data", "unclear"},
+    {"context-dependent", "22", "not defined", "4"},
+    {"Return it to them", "Keep it", "Scan for fingerprints", "Run analysis"},
+    {"Nothing", "Gasoline", "Fresh bread", "I don’t process smell"},
+    {"Feel awkwardComfort them", "Comfort them", "Record data", "Remain unaffected"}
+    }
+
+    ' {Human points, AI points}
+    Dim scores As Integer(,,) = {
+    {{5, 0}, {2, 3}, {1, 4}, {0, 5}},
+    {{2, 3}, {5, 0}, {1, 4}, {0, 5}},
+    {{0, 5}, {3, 2}, {5, 0}, {1, 4}},
+    {{5, 0}, {2, 2}, {0, 5}, {1, 4}},
+    {{1, 4}, {0, 5}, {2, 3}, {5, 0}},
+    {{5, 0}, {0, 5}, {1, 4}, {2, 3}},
+    {{1, 4}, {2, 3}, {5, 0}, {0, 5}},
+    {{2, 3}, {5, 0}, {0, 5}, {1, 4}}
+}
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadQuestion()
+    End Sub
+
+    Private Sub LoadQuestion()
+        lblQuestion.Text = questions(currentQuestion)
+        RadioButton1.Text = answers(currentQuestion, 0)
+        RadioButton2.Text = answers(currentQuestion, 1)
+        RadioButton3.Text = answers(currentQuestion, 2)
+        RadioButton4.Text = answers(currentQuestion, 3)
+
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+        RadioButton4.Checked = False
+
+        responseTime = 0
+        responseTimer.Start()
+    End Sub
+
+    Private Sub btnNextQuestion_Click(sender As Object, e As EventArgs) Handles btnNextQuestion.Click
+        Dim selected As Integer = -1
+        If RadioButton1.Checked Then selected = 0
+        If RadioButton2.Checked Then selected = 1
+        If RadioButton3.Checked Then selected = 2
+        If RadioButton4.Checked Then selected = 3
+
+        If selected = -1 Then
+            MessageBox.Show("Please select an answer.")
+            Exit Sub
+        End If
+
+        responseTimer.Stop()
+        If responseTime <= 2 Then
+            aiScore += 2
+            fastAnswerDetected = True
+        End If
+
+        humanScore += scores(currentQuestion, selected, 0)
+        aiScore += scores(currentQuestion, selected, 1)
+
+        currentQuestion += 1
+
+        If currentQuestion < questions.Length Then
+            LoadQuestion()
+        Else
+            ShowResult()
+        End If
+    End Sub
+
+    Private Sub ShowResult()
+        Dim resultText As String
+        If humanScore > aiScore Then
+            resultText = "You are human! 🧍 (" & humanScore & " : " & aiScore & ")"
+        ElseIf aiScore > humanScore Then
+            resultText = "Hmm... you might be an AI 🤖 (" & aiScore & " : " & humanScore & ")"
+        Else
+            resultText = "Undetermined. Maybe you're a cyborg? ⚙️"
+        End If
+
+        If fastAnswerDetected Then
+            resultText &= vbCrLf & "⚠️ Rapid responses detected!"
+        End If
+
+        MessageBox.Show(resultText)
+        btnNextQuestion.Enabled = False
+    End Sub
+
+    Private Sub responseTimer_Tick(sender As Object, e As EventArgs) Handles responseTimer.Tick
+        responseTime += 1
+    End Sub
+
+End Class
